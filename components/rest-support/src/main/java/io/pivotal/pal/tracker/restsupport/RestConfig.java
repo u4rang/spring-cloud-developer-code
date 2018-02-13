27428563a446fcacf0493c1c426627fb4450606a
Bill Kable <bkable@pivotal.io>
2018-02-13 17:33:09 -0600

    implement service discovery with eureka

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
@@ -2,6 +2,7 @@ package io.pivotal.pal.tracker.restsupport;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.netflix.discovery.EurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestOperations;
@@ -10,10 +11,20 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestConfig {
+    private final EurekaClient eurekaClient;
+
+    public RestConfig(EurekaClient eurekaClient) {
+        this.eurekaClient = eurekaClient;
+    }
 
     @Bean
     public RestOperations restOperations() {
         return new RestTemplate();
+    }
+
+    @Bean
+    ServiceLocator getServiceLocator() {
+        return new EurekaServiceLocator(eurekaClient);
     }
 
     @Bean
