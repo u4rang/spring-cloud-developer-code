00b760a74a43ad8c2b6884ccfe1c43ca19103f0b
Mike Gehard <mgehard@pivotal.io>
2018-02-15 15:44:03 -0700

    Change over to Turbine Stream

--- platform-services/turbine-server/src/main/java/io/pivotal/pal/turbineserver/TurbineServerApplication.java
+++ platform-services/turbine-server/src/main/java/io/pivotal/pal/turbineserver/TurbineServerApplication.java
@@ -2,10 +2,10 @@ package io.pivotal.pal.turbineserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 @SpringBootApplication
-@EnableTurbine
+@EnableTurbineStream
 public class TurbineServerApplication {
 
 	public static void main(String[] args) {
